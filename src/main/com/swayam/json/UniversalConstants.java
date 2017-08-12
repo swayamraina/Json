a9c2b67b608e18219b295cc1965b596fb5fe8e05
Swayam Raina <code.monk.wi@gmail.com>
2017-08-12 11:39:37 +0530

    Added global constants

---
+++ src/main/com/swayam/json/UniversalConstants.java
@@ -0,0 +1,18 @@
+package main.com.swayam.json;
+
+public interface UniversalConstants {
+	
+	static final char OPEN_SQUARE_BRACE = '[';
+	static final char CLOSED_SQUARE_BRACE = ']';
+	static final char CLOSED_CURLY_BRACE = '}';
+	static final char OPEN_CURLY_BRACE = '{';
+	static final char QUOTE = '"';
+	static final char COMMA = ',';
+	static final char COLON = ':';
+	static final char SPACE = ' ';
+	static final char NEWLINE = '\n';
+	static final char TAB = '\t';
+
+}
+
+
