814f5aa0d4594ad8a3b665a6ab7d52cd9ed9635e
Swayam Raina <code.monk.wi@gmail.com>
2017-09-12 22:30:57 +0530

    Added test case for primitive length computation

--- src/test/com/swayam/json/JsonTokenizerTest.java
+++ src/test/com/swayam/json/JsonTokenizerTest.java
@@ -1,5 +1,7 @@
 package test.com.swayam.json;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 import main.com.swayam.json.JsonObject;
@@ -19,6 +21,30 @@ public class JsonTokenizerTest {
 		String text = "{\"id\":\"12345\",\"name\":{\"first\":\"swayam\",\"last\":\"raina\"},\"level\":\"advanced\"}";
 		JsonObject json = new JsonTokenizer().tokenize(text);
 		System.out.println(json.prettify(text));
+	}
+	
+	@Test
+	public void testTokenize3() {
+		String text = "{\"name\":{\"last\":{\"name\":\"raina\"}},\"level\":\"advanced\"}";
+		JsonObject json = new JsonTokenizer().tokenize(text);
+		System.out.println(json.prettify(text));
+	}
+	
+	@Test
+	public void testComputeLengthForPrimitives() {
+		int t1=100;
+		float t2=1.032f;
+		boolean t3=true;
+		String t4="hello";
+		
+		Object o = t1;
+		assertEquals(3, JsonObject.computeLengthForPrimitives(o));
+		o = t2;
+		assertEquals(5, JsonObject.computeLengthForPrimitives(o));
+		o = t3;
+		assertEquals(4, JsonObject.computeLengthForPrimitives(o));
+		o = t4;
+		assertEquals(7, JsonObject.computeLengthForPrimitives(o));
 	}
 	
 }
