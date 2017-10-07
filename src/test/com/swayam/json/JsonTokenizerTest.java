99e41c75dd397270eed3d5b2e16195deffd74332
Swayam Raina <code.monk.wi@gmail.com>
2017-10-07 12:52:12 +0530

    Updated test cases

--- src/test/com/swayam/json/JsonTokenizerTest.java
+++ src/test/com/swayam/json/JsonTokenizerTest.java
@@ -37,7 +37,6 @@ public class JsonTokenizerTest {
 		JsonObject json = new JsonTokenizer().tokenize(text);
 		System.out.println(json.prettify());
 		System.out.println();
-		// may fail because of internal rearrangement
 		assertEquals(text, json.serialize());
 	}
 	
@@ -47,70 +46,48 @@ public class JsonTokenizerTest {
 		JsonObject json = new JsonTokenizer().tokenize(text);
 		System.out.println(json.prettify());
 		System.out.println();
-		// may fail because of internal rearrangement
 		assertEquals(text, json.serialize());
 	}
 	
 	@Test
-	public void testComputeLengthForPrimitives() {
-		int t1=100;
-		float t2=1.032f;
-		boolean t3=true;
-		String t4="hello";
-		
-		Object o = t1;
-		assertEquals(3, JsonObject.computeLengthForPrimitives(o));
-		o = t2;
-		assertEquals(5, JsonObject.computeLengthForPrimitives(o));
-		o = t3;
-		assertEquals(4, JsonObject.computeLengthForPrimitives(o));
-		o = t4;
-		assertEquals(7, JsonObject.computeLengthForPrimitives(o));
-	}
-	
-	@Test
 	public void testTokenizeWithRecursiveArray() {
-		String text = "{\"roomamtes\":[[\"swayam\", \"gokul\"],[\"jayesh\"],[\"rahul\"]]}";
+		String text = "{\"roomamtes\":[[\"swayam\",\"gokul\"],[\"jayesh\"],[\"rahul\"]]}";
 		JsonObject json = new JsonTokenizer().tokenize(text);
 		System.out.println(json.prettify());
 		System.out.println();
-		// may fail because of internal rearrangement or space trimming
 		assertEquals(text, json.serialize());
 	}
 	
 	@Test
 	public void testFloatPrimitiveInJson() {
-		String text = "{\"cost\": 100.00}";
+		String text = "{\"cost\":100.06}";
 		JsonObject json = new JsonTokenizer().tokenize(text);
 		System.out.println(json.prettify());
 		System.out.println();
-		// may fail because of internal rearrangement or space trimming
 		assertEquals(text, json.serialize());
 	}
 	
 	@Test
 	public void testIntegerPrimitiveInJson() {
-		String text = "{\"age\" : 23}";
+		String text = "{\"age\":23}";
 		JsonObject json = new JsonTokenizer().tokenize(text);
 		System.out.println(json.prettify());
 		System.out.println();
-		// may fail because of internal rearrangement or space trimming
 		assertEquals(text, json.serialize());
 	}
 	
 	@Test
 	public void testNegativeIntegerPrimitiveInJson() {
-		String text = "{\"loss percent\" : -23}";
+		String text = "{\"loss percent\":-23}";
 		JsonObject json = new JsonTokenizer().tokenize(text);
 		System.out.println(json.prettify());
 		System.out.println();
-		// may fail because of internal rearrangement or space trimming
 		assertEquals(text, json.serialize());
 	}
 	
 	@Test(expected = JsonException.class)
 	public void testIntegerPrimitiveInJsonNegative() {
-		String text = "{\"age\" : 23f}";
+		String text = "{\"age\":23f}";
 		JsonObject json = new JsonTokenizer().tokenize(text);
 		System.out.println(json.prettify());
 		System.out.println();
@@ -118,7 +95,7 @@ public class JsonTokenizerTest {
 	
 	@Test(expected = JsonException.class)
 	public void testNegativeIntegerPrimitiveInJsonNegative() {
-		String text = "{\"age\" : -23f}";
+		String text = "{\"age\":-23f}";
 		JsonObject json = new JsonTokenizer().tokenize(text);
 		System.out.println(json.prettify());
 		System.out.println();
@@ -126,17 +103,16 @@ public class JsonTokenizerTest {
 	
 	@Test
 	public void testTruePrimitiveInJson() {
-		String text = "{\"eligible\": true}";
+		String text = "{\"eligible\":true}";
 		JsonObject json = new JsonTokenizer().tokenize(text);
 		System.out.println(json.prettify());
 		System.out.println();
-		// may fail because of internal rearrangement or space trimming
 		assertEquals(text, json.serialize());
 	}
 	
 	@Test(expected = JsonException.class)
 	public void testIncorrectTruePrimitiveInJson() {
-		String text = "{\"eligible\": -true}";
+		String text = "{\"eligible\":-true}";
 		JsonObject json = new JsonTokenizer().tokenize(text);
 		System.out.println(json.prettify());
 		System.out.println();
@@ -144,7 +120,7 @@ public class JsonTokenizerTest {
 	
 	@Test(expected = JsonException.class)
 	public void testInvalidStringPrimitiveInJsonNegative() {
-		String text = "{\"eligible\": truea}";
+		String text = "{\"eligible\":truea}";
 		JsonObject json = new JsonTokenizer().tokenize(text);
 		System.out.println(json.prettify());
 		System.out.println();
@@ -152,11 +128,10 @@ public class JsonTokenizerTest {
 	
 	@Test
 	public void testFalsePrimitiveInJson() {
-		String text = "{\"eligible\": false }";
+		String text = "{\"eligible\":false}";
 		JsonObject json = new JsonTokenizer().tokenize(text);
 		System.out.println(json.prettify());
 		System.out.println();
-		// may fail because of internal rearrangement or space trimming
 		assertEquals(text, json.serialize());
 	}
 	
@@ -166,54 +141,24 @@ public class JsonTokenizerTest {
 		JsonObject json = new JsonTokenizer().tokenize(text);
 		System.out.println(json.prettify());
 		System.out.println();
-		// may fail because of internal rearrangement or space trimming
 		assertEquals(text, json.serialize());
 	}
 	
 	@Test
 	public void testObjectsInArray() {
-		String text = "{\"class\":[{\"roll1\":{\"fname\":\"swayam\",\"lname\":\"raina\"}},{\"roll2\":{\"fname\":\"ujjwal\",\"lname\":\"raina\"}}],\"total\":2}";
+		String text = "{\"total\":2,\"class\":[{\"roll1\":{\"fname\":\"swayam\",\"lname\":\"raina\"}},{\"roll2\":{\"fname\":\"ujjwal\",\"lname\":\"raina\"}}]}";
 		JsonObject json = new JsonTokenizer().tokenize(text);
 		System.out.println(json.prettify());
 		System.out.println();
-		// may fail because of internal rearrangement or space trimming
 		assertEquals(text, json.serialize());
-	}
-	
-	@Test
-	public void testStandAlonePrettify() {
-		String text = "{\"total\":2,\"class\":[{\"roll1\":{\"fname\":\"swayam\",\"lname\":\"raina\"}},{\"roll2\":{\"fname\":\"ujjwal\",\"lname\":\"raina\"}}]}";
-		System.out.println(JsonObject.prettify(text));
-		System.out.println();
-	}
-	
-	@Test
-	public void compareStandAlonePrettify() {
-		String text = "{\"total\":2,\"class\":[{\"roll1\":{\"fname\":\"swayam\",\"lname\":\"raina\"}},{\"roll2\":{\"fname\":\"ujjwal\",\"lname\":\"raina\"}}]}";
-		String standAlone = JsonObject.prettify(text);
-		String dependent = new JsonTokenizer().tokenize(text).prettify();
-		// may fail because tokenizer internally changes
-		// the ordering of the inner elements
-		assertEquals(dependent, standAlone);
 	}
 	
 	@Test
 	public void testJsonArrayWithPrimitives() {
-		String text = "{\"numbers\": [1,2,3,4,5,6,7,8,9] }";
+		String text = "{\"numbers\":[1,2,3,4,5,6,7,8,9]}";
 		JsonObject json = new JsonTokenizer().tokenize(text);
 		System.out.println(json.prettify());
 		System.out.println();
-		// may fail because of internal rearrangement or space trimming
-		assertEquals(text, json.serialize());
-	}
-	
-	@Test
-	public void testString() {
-		String text = "{\"address\": \"347 Everit Street, Cotopaxi, Florida, 215\"}";
-		JsonObject json = new JsonTokenizer().tokenize(text);
-		System.out.println(json.prettify());
-		System.out.println();
-		// may fail because of internal rearrangement or space trimming
 		assertEquals(text, json.serialize());
 	}
 	
